95a8ff21531eee3ec8c66ac16313d10eef2411f4
BoxFort <hello@boxfort.co>
2022-09-02 19:32:05 -0500

    Initial Commit

---
+++ Boxfort Plus/Boxfort_PlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  Boxfort_PlusApp.swift
+//  Boxfort Plus
+//
+//  Created by Matthew Ryan on 9/2/22.
+//
+
+import SwiftUI
+
+@main
+struct Boxfort_PlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Boxfort Plus/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Boxfort Plus
+//
+//  Created by Matthew Ryan on 9/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
