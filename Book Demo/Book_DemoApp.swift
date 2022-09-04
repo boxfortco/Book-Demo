c2a5c824232813d4dd7c3ec316ab5c221eebdd2c
BoxFort <hello@boxfort.co>
2022-09-04 13:05:10 -0500

    Removed splash view

--- Book Demo/Book_DemoApp.swift
+++ Book Demo/Book_DemoApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Book_DemoApp: App {
     var body: some Scene {
         WindowGroup {
-            SplashView()
+            ContentView(tabViewRouter: TabViewRouter())
         }
     }
 }
